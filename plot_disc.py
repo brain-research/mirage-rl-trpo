a2bfaa6f67c19accb995baa348b414c7e9bd92d3
George Tucker <gjt@google.com>
2018-06-27 14:55:52 -0700

    Add runner scripts + results

--- plot_disc.py
+++ plot_disc.py
@@ -18,24 +18,28 @@ rc('text', usetex=True)
 color_list = sns.color_palette("muted")
 sns.palplot(color_list)
 
-log_dir = 'logs'
+log_dir = 'gs_results'
 batch_size = 25000
-fnames = glob.glob('{}/**/*.txt'.format(log_dir), recursive=True)
+fnames = glob.glob('{}/*.txt'.format(log_dir), recursive=True)
 
-colors = {}
-for i, thing in enumerate(['none', 'v', 'vh0', 'vh1']):
-  colors[thing] = color_list[i]
+envs = ['HalfCheetah-v1', 'Walker2d-v1', 'Humanoid-v1'] #'Walker2d-v1'
+baselines = ['v', 'horizon_v', 'time_v']
+colors = dict(zip(baselines, color_list[:len(baselines)]))
 
-map_baselines = {'none': 'mse_none', 'v': 'mse_v', 'vh0': 'mse_h0', 'vh1': 'mse_h1'}
-
-# 'logs/Walker2d-v1/vh0_1002.txt'
+savgol_window = 21
+smooth = lambda x: savgol_filter(x, savgol_window, 5)
 
 results = {}
 variances = {}
 
 for fname in fnames:
-  _, env, exp = fname.split('/')
-  baseline, seed = os.path.splitext(exp)[0].split('_')
+  exp = os.path.splitext(os.path.basename(fname))[0].split('_')
+  env = exp[0]
+  seed = exp[-1]
+  if len(exp) == 5:
+    baseline = 'v'
+  else:
+    baseline = '%s_v' % exp[2]
 
   data = []
   with open(fname, 'r') as f:
@@ -47,59 +51,59 @@ for fname in fnames:
     variances[env] = {}
   if baseline not in results[env]:
     results[env][baseline] = []
-    variances[env][baseline] = []
+    variances[env][baseline] = {}
 
   rewards = [x['reward_batch'] for x in data]
-  var = [x[map_baselines[baseline]] for x in data]
   results[env][baseline].append(rewards)
-  variances[env][baseline].append(var)
-
-
-envs = ['HalfCheetah-v1', 'Walker2d-v1', 'Humanoid-v1']
-envs_titles = {'halfcheetah': 'HalfCheetah-v1', 'walker2d': 'Walker2d-v1', 'humanoid': 'Humanoid-v1'}
-baselines = ['none', 'v', 'vh0', 'vh1']
-
-savgol_window = 21
+  for var_baseline in baselines:
+    var = [x['var_%s' % var_baseline] for x in data]
+    variances[env][baseline][var_baseline] = var
 
 # Plot rewards
-fig, axes = plt.subplots(1, len(envs), figsize=(20, 6))
+def plot(data, ylabel, out):
+  fig, axes = plt.subplots(1, len(envs), figsize=(20, 6))
 
-for envidx, env in enumerate(envs):
-  ax = axes[envidx]
-  for i, baseline in enumerate(baselines):
-    doto = np.array(results[env][baseline])
-    min_len = len(doto[0])
-    doto_z1 = savgol_filter(doto.mean(0) + doto.std(0), savgol_window, 5)
-    doto_z_1 = savgol_filter(doto.mean(0) - doto.std(0), savgol_window, 5)
-    doto_max = savgol_filter(doto.max(0), savgol_window, 5)
-    doto_min = savgol_filter(doto.min(0), savgol_window, 5)
-    doto_mean = savgol_filter(doto.mean(0), savgol_window, 5)
+  for envidx, env in enumerate(envs):
+    ax = axes[envidx]
+    for i, baseline in enumerate(baselines):
+      doto = data[env][baseline]
+      min_len = min(map(len, doto))
+      doto = [x[:min_len] for x in doto]
+      doto = np.array(doto)
+      doto_z1 = savgol_filter(doto.mean(0) + doto.std(0), savgol_window, 5)
+      doto_z_1 = savgol_filter(doto.mean(0) - doto.std(0), savgol_window, 5)
+      doto_max = savgol_filter(doto.max(0), savgol_window, 5)
+      doto_min = savgol_filter(doto.min(0), savgol_window, 5)
+      doto_mean = savgol_filter(doto.mean(0), savgol_window, 5)
 
-    ax.plot(np.arange(min_len) * 25, doto_mean, color=colors[baseline], label=baseline)
-    ax.fill_between(np.arange(min_len) * 25, doto_mean, np.where(doto_z1 > doto_max, doto_max, doto_z1), color=colors[baseline], alpha=0.2)
-    ax.fill_between(np.arange(min_len) * 25, np.where(doto_z_1 < doto_min, doto_min, doto_z_1), doto_mean, color=colors[baseline], alpha=0.2)
+      ax.plot(np.arange(min_len) * 25, doto_mean, color=colors[baseline])
+      ax.fill_between(np.arange(min_len) * 25, doto_mean, np.where(doto_z1 > doto_max, doto_max, doto_z1), color=colors[baseline], alpha=0.2)
+      ax.fill_between(np.arange(min_len) * 25, np.where(doto_z_1 < doto_min, doto_min, doto_z_1), doto_mean, color=colors[baseline], alpha=0.2)
 
-  ax.tick_params(axis='both', which='major', labelsize=11)
-  ax.tick_params(axis='both', which='minor', labelsize=11)
+    ax.tick_params(axis='both', which='major', labelsize=11)
+    ax.tick_params(axis='both', which='minor', labelsize=11)
 
-  ax.set_xlabel('Steps (thousands)')
-  ax.set_ylabel('Average Reward')
+    ax.set_xlabel('Steps (thousands)')
+    ax.set_ylabel(ylabel)
 
-  ax.grid(alpha=0.5)
-  ax.set_title("{}".format(env), fontsize=18)
+    ax.grid(alpha=0.5)
+    ax.set_title("{}".format(env), fontsize=18)
 
-h1 = mpatches.Patch(color=color_list[0], label='TRPO (no baseline)')
-h2 = mpatches.Patch(color=color_list[1], label='TRPO (V(s) baseline)')
-h3 = mpatches.Patch(color=color_list[2], label='TRPO (simple horizon-aware V(s) baseline)')
-h4 = mpatches.Patch(color=color_list[3], label='TRPO (horizon-aware V(s) baseline)')
-leg = fig.legend(handles=[h4, h3, h2, h1], loc='lower center', ncol=2, prop={'size': 14})
+  legend_handles = [
+    mpatches.Patch(color=colors['v'], label='Value baseline'),
+    mpatches.Patch(color=colors['horizon_v'], label='Horizon-aware value baseline'),
+    mpatches.Patch(color=colors['time_v'], label='Value with timeleft baseline'),
+  ]
+  leg = plt.legend(handles=legend_handles, loc='lower center', ncol=2, prop={'size': 14})
 
-bb = leg.get_bbox_to_anchor().inverse_transformed(ax.transAxes)
-bb.y0 += -0.15
-leg.set_bbox_to_anchor(bb, transform = ax.transAxes)
+  bb = leg.get_bbox_to_anchor().inverse_transformed(ax.transAxes)
+  bb.y0 += -0.25
+  bb.x0 -= 2.4
+  leg.set_bbox_to_anchor(bb, transform = ax.transAxes)
 
-fig.savefig('plots/disc_value_reward.pdf', bbox_inches='tight', format='pdf', dpi=200)
+  fig.savefig('plots/%s.pdf' % out, bbox_inches='tight', format='pdf', dpi=200)
 
+plot(results, 'Average Reward', 'reward')
 
 # Plot variances
 fig, axes = plt.subplots(1, len(envs), figsize=(20, 6))
@@ -107,38 +111,30 @@ fig, axes = plt.subplots(1, len(envs), figsize=(20, 6))
 for envidx, env in enumerate(envs):
   ax = axes[envidx]
   for i, baseline in enumerate(baselines):
-    doto = np.array(variances[env][baseline])
-    min_len = len(doto[0])
-    doto_z1 = savgol_filter(doto.mean(0) + doto.std(0), savgol_window, 5)
-    doto_z_1 = savgol_filter(doto.mean(0) - doto.std(0), savgol_window, 5)
-    doto_max = savgol_filter(doto.max(0), savgol_window, 5)
-    doto_min = savgol_filter(doto.min(0), savgol_window, 5)
-    doto_mean = savgol_filter(doto.mean(0), savgol_window, 5)
-
-    ax.plot(np.arange(min_len) * 25, doto_mean, color=colors[baseline], label=baseline)
-    ax.fill_between(np.arange(min_len) * 25, doto_mean, np.where(doto_z1 > doto_max, doto_max, doto_z1), color=colors[baseline], alpha=0.2)
-    ax.fill_between(np.arange(min_len) * 25, np.where(doto_z_1 < doto_min, doto_min, doto_z_1), doto_mean, color=colors[baseline], alpha=0.2)
+    doto = np.array(variances[env]['v'][baseline])
+    ax.plot(np.arange(len(doto)) * 25, smooth(doto), color=colors[baseline], label=baseline)
 
   ax.tick_params(axis='both', which='major', labelsize=11)
   ax.tick_params(axis='both', which='minor', labelsize=11)
 
   ax.set_xlabel('Steps (thousands)')
-  ax.set_ylabel('MSE')
+  ax.set_ylabel('Variance')
 
   ax.grid(alpha=0.5)
   ax.set_title("{}".format(env), fontsize=18)
 
-# @Shane: Create mpatches to fill in the colors (make sure they correspond to the ones you used)
-h1 = mpatches.Patch(color=color_list[0], label='TRPO (no baseline)')
-h2 = mpatches.Patch(color=color_list[1], label='TRPO (V(s) baseline)')
-h3 = mpatches.Patch(color=color_list[2], label='TRPO (simple horizon-aware V(s) baseline)')
-h4 = mpatches.Patch(color=color_list[3], label='TRPO (horizon-aware V(s) baseline)')
-leg = fig.legend(handles=[h4, h3, h2, h1], loc='lower center', ncol=2, prop={'size': 14})
+legend_handles = [
+  mpatches.Patch(color=colors['v'], label='Value baseline'),
+  mpatches.Patch(color=colors['horizon_v'], label='Horizon-aware value baseline'),
+  mpatches.Patch(color=colors['time_v'], label='Value with timeleft baseline'),
+]
+leg = plt.legend(handles=legend_handles, loc='lower center', ncol=2, prop={'size': 14})
 
 # @Shane: Use to adjust the legend down.
 bb = leg.get_bbox_to_anchor().inverse_transformed(ax.transAxes)
-bb.y0 += -0.15
+bb.y0 += -0.25
+bb.x0 -= 2.4
 leg.set_bbox_to_anchor(bb, transform = ax.transAxes)
 
-fig.savefig('plots/disc_value_variance.pdf', bbox_inches='tight', format='pdf', dpi=200)
+fig.savefig('plots/variance.pdf', bbox_inches='tight', format='pdf', dpi=200)
 
---
+++ run_horizon_time_comparison.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for SEED in `seq 1 5`
+do
+  for ENV in Walker2d-v1 HalfCheetah-v1 Humanoid-v1
+  do
+    for BASELINE in v horizon_v time_v
+    do
+      berg run -i 'echo Starting' "mkdir -p /home/gjt/berg_results && python main.py --env-name $ENV --baseline ${BASELINE} --gamma 0.99 --tau 1.00 --batch-size 25000 --n-epochs 200 --max-time 1000 --seed $SEED --log-file /home/gjt/berg_results/${ENV}_baseline_${BASELINE}_seed_${SEED}.txt"
+    done
+  done
+done

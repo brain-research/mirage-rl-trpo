08f46ae47a40b802ca2f5664a4cfcb506816b0ef
George Tucker <gjt@google.com>
2018-06-27 15:13:49 -0700

    Add license header

--- run_horizon_time_comparison.sh
+++ run_horizon_time_comparison.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright 2018 Google LLC
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
 for SEED in `seq 1 5`
 do
   for ENV in Walker2d-v1 HalfCheetah-v1 Humanoid-v1

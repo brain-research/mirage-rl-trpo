a4e486c3968c956d9a4b42a31e917ef673c2055e
George Tucker <gjt@google.com>
2018-06-29 13:07:14 -0700

    Removing extra files.

--- plot_gcp3.py
+++ plot_gcp3.py
@@ -98,39 +98,3 @@ h4 = mpatches.Patch(color=color_list[3], label='No baseline')
 leg = fig.legend(handles=[h3, h4, h1, h2], loc='lower center', ncol=4, prop={'size': 10})
 
 fig.savefig('disc_value_mean_stds.png')
-quit(1)
-
-# Plot reward for lbfg/y
-plt.figure()
-i = 0
-for k, v in results.items():
-  if k[1] == 'lbfgs' and k[2] == 'y':
-    y = [[row['reward_batch'] for row in res] for res in v]
-
-    for j, res in enumerate(y):
-      plt.plot(np.arange(len(res)) * 5, ema(res, 0.95), color=colors[i],
-               label='-'.join(k) if j == 0 else None)
-    i += 1
-  plt.legend()
-  plt.xlabel('Steps (thousands)')
-  plt.ylabel('Reward')
-  plt.title(control_alg)
-  plt.savefig('plots/reward_lbfgs_y.png')
-
-for k, v in results.items():
-  plt.figure()
-  fields = ['mse_none', 'mse_v', 'mse_q', 'mse_model']
-
-  for i, res in enumerate(v):
-    plt.subplot(len(v), 1, i+1)
-    for j, field in enumerate(fields):
-      y = [row[field]/row['mse_none'] for row in res]
-      plt.plot(np.arange(len(y)) * 5, ema(y, 0.95, 1.), color=colors[j], label=field)
-      plt.ylim(0.95, 1.05)
-
-  plt.legend()
-  plt.xlabel('Steps (thousands)')
-  plt.ylabel('MSE relative to no baseline')
-  plt.title('-'.join(k))
-  plt.savefig('plots/mse_%s.png' % ('-'.join(k)))
-
